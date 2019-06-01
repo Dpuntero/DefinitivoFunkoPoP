8081c5fe875557105e7fd98abddb6e63e64d3850
Maria Mera <mariameraf@gmail.com>
2019-06-01 16:38:59 +0200

    Creación de nuevos productos

--- AppFunkoPop/Controllers/ProductoController.cs
+++ AppFunkoPop/Controllers/ProductoController.cs
@@ -1,17 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using AppFunkoPop.Models;
 
-namespace ProyectoDawFunko.Controllers
+
+namespace AppFunkoPop.Controllers
 {
     public class ProductoController : Controller
     {
         // GET: Producto
-        public ActionResult Index()
+        public ActionResult Productos()
+        {
+            /* List<PRODUCTO> listaProductos;
+             using (Database1Entities db = new Database1Entities())
+             {
+                 listaProductos  = (from prod in db.PRODUCTOes
+                              select new PRODUCTO
+                              {
+                                  PRODUCTO_ID = prod.PRODUCTO_ID,
+                                  NOMBREP = prod.NOMBREP,
+                                  PRECIO = prod.PRECIO,
+                                  DESCRIP = prod.DESCRIP,
+                                  CATEGORIA = prod.CATEGORIA,
+                                  UD_DISPO = prod.UD_DISPO,
+                                  V_ID = prod.V_ID,
+                                  IMAGEN = prod.IMAGEN,
+                                  LINK = prod.LINK
+                              }).ToList();
+             }*/
+            return View();
+        }
+
+        public ActionResult NuevoProducto()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult NuevoProducto(AppFunkoPop.Models.PRODUCTO nuevoProd)
+        {
+            //try{
+            //   System.Diagnostics.Debug.WriteLine("hola");
+            // if (ModelState.IsValid)
+            //{
+            //  System.Diagnostics.Debug.WriteLine("Entra en el if");
+            using (Database1Entities db = new Database1Entities())
+            {
+
+                db.PRODUCTOes.Add(nuevoProd);
+                db.SaveChanges();
+            }
+            
+            return Redirect("~/Producto/Productos");
+                //}
+               //return View("Productos");
+            //}catch(Exception ex)
+            //{
+                
+              //  throw new Exception(ex.Message);
+            //}
         }
     }
 }
--- AppFunkoPop/Models/PRODUCTO.cs
+++ AppFunkoPop/Models/PRODUCTO.cs
@@ -11,7 +11,8 @@ namespace AppFunkoPop.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class PRODUCTO
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -21,15 +22,41 @@ namespace AppFunkoPop.Models
             this.PEDIDOPRODUCTOes = new HashSet<PEDIDOPRODUCTO>();
             this.PROVEEDORs = new HashSet<PROVEEDOR>();
         }
-    
+
+       /* [Required]
+        [Display(Name = "ID")]*/
         public int PRODUCTO_ID { get; set; }
+
+        [Required]
+        [Display(Name = "Nombre")]
         public string NOMBREP { get; set; }
+
+        [Required]
+        [Display(Name = "Precio")]
         public decimal PRECIO { get; set; }
+
+        [Required]
+        [Display(Name = "Descripcion")]
         public string DESCRIP { get; set; }
+
+        [Required]
+        [Display(Name = "Categoria")]
         public string CATEGORIA { get; set; }
+
+        [Required]
+        [Display(Name = "Unidades")]
         public Nullable<int> UD_DISPO { get; set; }
+
+        [Required]
+        [Display(Name = "Proveedor")]
         public Nullable<int> V_ID { get; set; }
+
+        [Required]
+        [Display(Name = "Imagen")]
         public string IMAGEN { get; set; }
+
+        [Required]
+        [Display(Name = "link")]
         public string LINK { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
