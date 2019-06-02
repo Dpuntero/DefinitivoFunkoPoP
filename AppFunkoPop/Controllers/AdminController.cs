cbfc46823c24e9cae66578a7bbbedeb0b7b53901
Maria Mera <mariameraf@gmail.com>
2019-06-02 19:19:45 +0200

    Subo la parte de listartodos los productos y acceder a la ventana de edición de cada uno. (esta no esta operativa aun)

--- AppFunkoPop/Controllers/AdminController.cs
+++ AppFunkoPop/Controllers/AdminController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using AppFunkoPop.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -27,12 +29,39 @@ namespace ProyectoDawFunko.Controllers
             return View();
         }
 
+        //METODOS PARA GESTION DE PRODUCTOS
+        // GET: PRODUCTOes
         public ActionResult GestionProductos()
         {
-            ViewBag.Message = "Your contact page.. Nueva!";
-
+            Database1Entities db = new Database1Entities();
+            ViewBag.Productos = db.PRODUCTOes.ToList();
             return View();
         }
+
+        // GET: PRODUCTOes/EditarProducto/5
+
+        public ActionResult EditarProducto(int? id)
+        {
+            Database1Entities db = new Database1Entities();
+            PRODUCTO pRODUCTO = db.PRODUCTOes.Find(id);
+
+            return View("EditadoProducto", pRODUCTO);
+        }
+       
+
+        [HttpPost]
+
+        public ActionResult EditadoProducto(PRODUCTO modificado)
+        {
+            //ESTE METODO TODAVIA NO FUNCIONA BIEN. NO ACTUALIZAEL REGISTRO
+            Database1Entities db = new Database1Entities();
+            db.Entry(modificado).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("GestionProductos");
+
+        }
+
+        //FIN METODOS PRODUCTOS
 
         public ActionResult GestionPedidos()
         {
