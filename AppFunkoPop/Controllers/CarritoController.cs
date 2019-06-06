c15cf48bdb19327290d33af5bcb38a4b2ff29a26
puntero <dpuntero@gmail.com>
2019-06-06 21:30:57 +0200

    Controld e lenguaje acabado, falta insertar traducciones. Arreglos en el password. Cambio de contraseña. Catalogo filtrado por categorias ok. Vista correcta del detalle de los productos. Vista corecta cuando insertas url invalida

--- AppFunkoPop/Controllers/CarritoController.cs
+++ AppFunkoPop/Controllers/CarritoController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AppFunkoPop.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,39 @@ namespace ProyectoDawFunko.Controllers
 {
     public class CarritoController : Controller
     {
-        // GET: Carrito
-        public ActionResult InicioCarrito()
+        public ActionResult CreacionCarrito()
         {
-            return View();
+            using (Database1Entities db = new Database1Entities())
+            {
+                CARRITO nuevoCarrito = new CARRITO();
+                nuevoCarrito.FECHA_CR = DateTime.Today;
+                nuevoCarrito.USUARIO_ID = Convert.ToInt32(Session["USUARIO_ID"]);
+                nuevoCarrito.PRECIO_T = "1";
+                db.CARRITOes.Add(nuevoCarrito);
+                db.SaveChanges();
+            }
+
+
+                return View();
+        }
+  
+        public ActionResult AñadirProductoACarrito()
+        {
+            using (Database1Entities db = new Database1Entities())
+            {
+                int c = Convert.ToInt32(Session["USUARIO_ID"]);
+                var carritoDetails = db.CARRITOes.Where(x => x.USUARIO_ID ==c).FirstOrDefault();
+                if (carritoDetails != null)
+                {
+
+                }
+                else
+                {
+                    CreacionCarrito();
+                }
+            }
+
+                return RedirectToAction("Index","Home");
         }
     }
 }
