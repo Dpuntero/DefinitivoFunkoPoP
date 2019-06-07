308020d47f1afb80167585f33e8f45b347f23e7d
puntero <dpuntero@gmail.com>
2019-06-07 21:55:08 +0200

    Creación de cookies y carrito, acceso a carrito y clases pertinentes

---
+++ AppFunkoPop/Models/ProductoCookie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace AppFunkoPop.Models
+{
+    public class ProductoCookie
+    {
+        
+        public string producto_Id { get; set; }
+       
+        public string unidades { get; set; }
+    
+       
+
+    }
+}
